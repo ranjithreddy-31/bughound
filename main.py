24a24a2705f23f564b6caa50242da0c608d23a74
Ranjith Reddy Gaddam <ranjithreddy.gaddam01@student.csulb.edu>
2023-02-25 13:56:47 -0800

    completed employee part

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, redirect, url_for, request, render_template
 import mysql.connector
 app = Flask(__name__)
 
+def get_connection():
+	return mysql.connector.connect(user='root', password='', host='localhost', database='bughound')
 
 @app.route('/')
 def index():
@@ -21,19 +23,98 @@ def register_employee():
     password = request.form['password']
     userlevel = request.form['userlevel']
     # Connect to database
-    conn = mysql.connector.connect(user='root', password='',
-                                    host='localhost', database='bughound')
-    cursor = conn.cursor()
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
 
-    # Execute query
-    query = "INSERT INTO employees (name, username, password, userlevel) VALUES (%s, %s, %s, %s)"
-    values = (name, username, password, userlevel)
-    cursor.execute(query, values)
-    conn.commit()
-    cursor.close()
-    conn.close()
-    return "success"
+        # Execute query
+        query = "INSERT INTO employees (name, username, password, userlevel) VALUES (%s, %s, %s, %s)"
+        values = (name, username, password, userlevel)
+        cursor.execute(query, values)
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return "success"
+    except Exception as e:
+	    return f"failed:{e}"
+@app.route('/delete_employee_form')
+def delete_employee_form():
+	return render_template('delete_employee.html')
+@app.route('/delete_employee', methods=['POST'])
+def delete_employee():
+    # Fetch form data
+    username = request.form['username']
+    password = request.form['password']
+    employee_id = int(request.form['employee_id'])
+    # Connect to database
+    try:
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        # Execute query
+        query = f"select password from employees where username='{username}'"
+        cursor.execute(query)
+        db_password = cursor.fetchall()[0][0]
+        if password == db_password:
+            query = f"delete from employees where emp_id={employee_id}"
+            cursor.execute(query)
+            conn.commit()
+            cursor.close()
+            conn.close()
+            return f"Successfully deleted employee:{employee_id}"
+        else:
+            cursor.close()
+            conn.close()
+            return "Invalid credentials"
+    except Exception as e:
+	    return f"failed:No employee id in the database (tehnical reason:{e})"
+@app.route('/update_employee_form')
+def update_employee_form():
+	return render_template('update_employee.html')
 
 
+@app.route('/update_employee', methods=['POST'])
+def update_employee():
+	username = request.form['username']
+	password = request.form['password']
+	employee_id = int(request.form['employee_id'])
+	new_name = request.form['new_name']
+	new_username = request.form['new_username']
+	new_password = request.form['new_password']
+	try:
+		conn = get_connection()
+		cursor = conn.cursor()
+		query = f"select password from employees where username='{username}'"
+		cursor.execute(query)
+		db_password = cursor.fetchall()[0][0]
+		if password == db_password:
+			if new_name:
+				query = f"update employees set name='{new_name}' where emp_id = {employee_id}"
+				print(query)
+				cursor.execute(query)
+				conn.commit()
+			if new_username:
+				query = f"update employees set username='{new_username}' where emp_id = {employee_id}"
+				print(query)
+				cursor.execute(query)
+				conn.commit()
+			if new_password:
+				query = f"update employees set password='{new_password}' where emp_id = {employee_id}"
+				print(query)
+				cursor.execute(query)
+				conn.commit()
+			cursor.close()
+			conn.close()
+			return f"successfully updated employee:{employee_id}"
+		else:
+			cursor.close()
+			conn.close()
+			return "Failed updating: Invalid Password"
+
+	except Exception as e:
+		return f"failed:No employee id in the database (tehnical reason:{e})"
+
+		
+	
 if __name__ == '__main__':
 	app.run(debug=True)
